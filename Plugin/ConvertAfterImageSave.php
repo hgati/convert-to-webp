86baf7191afdcc99feaaf11ec1e0de292f405516
Dennis <cobays@gmail.com>
2024-05-31 17:18:11 +0900

    add log

--- Plugin/ConvertAfterImageSave.php
+++ Plugin/ConvertAfterImageSave.php
@@ -4,14 +4,17 @@ namespace Hgati\ConvertToWebp\Plugin;
 
 use Magento\Framework\Image;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Psr\Log\LoggerInterface;
 
 class ConvertAfterImageSave
 {
 	protected $scopeConfig;
+	protected $logger;
 
-    public function __construct(ScopeConfigInterface $scopeConfig)
+    public function __construct(ScopeConfigInterface $scopeConfig, LoggerInterface $logger)
     {
-        $this->scopeConfig = $scopeConfig;
+		$this->scopeConfig = $scopeConfig;
+        $this->logger = $logger;
     }
 
     /**
@@ -23,7 +26,10 @@ class ConvertAfterImageSave
     public function afterSave(Image $subject, $return, $destination = null)
     {
 		$isEnabled = (int)$this->scopeConfig->isSetFlag('hgati_converttowebp/general/enable', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-		if (!$isEnabled) return;
+		if (!$isEnabled) {
+			$this->logger->info('Hgati_ConvertToWebP:: Skipped due to disabled on system.xml');
+			return;
+		}
 
 		$sourceImagePath = $destination;
 		$imageInfo = pathinfo($sourceImagePath);
@@ -35,6 +41,7 @@ class ConvertAfterImageSave
 			$sourceImage = $extension === 'png' ? imagecreatefrompng($sourceImagePath) : imagecreatefromjpeg($sourceImagePath);
 			imagewebp($sourceImage, $webPImagePath, 65);
 			imagedestroy($sourceImage);
+			$this->logger->info("Hgati_ConvertToWebPp:: $webPImagePath (Quality: 65)");
 		}
     }
 }
