03673048f16d1bfc2be669bbb5d3ba758421841d
Dennis <cobays@gmail.com>
2024-05-31 16:51:24 +0900

    Init Repos.

---
+++ Plugin/ConvertAfterImageSave.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace Hgati\ConvertToWebp\Plugin;
+
+use Magento\Framework\Image;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class ConvertAfterImageSave
+{
+	protected $scopeConfig;
+
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @param Image $subject
+     * @param mixed $return
+     * @param null $destination
+     * @return void
+     */
+    public function afterSave(Image $subject, $return, $destination = null)
+    {
+		$isEnabled = $this->scopeConfig->isSetFlag('hgati_converttowebp/general/enable', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+		if (!$isEnabled) return;
+
+		$sourceImagePath = $destination;
+		$imageInfo = pathinfo($sourceImagePath);
+		if(!isset($imageInfo['extension'])) return;
+		$extension = strtolower($imageInfo['extension']);
+		if ($extension === 'jpg' || $extension === 'jpeg' || $extension === 'png') {
+			$webPImagePath = "$sourceImagePath.ngx.webp";
+
+			$sourceImage = $extension === 'png' ? imagecreatefrompng($sourceImagePath) : imagecreatefromjpeg($sourceImagePath);
+			imagewebp($sourceImage, $webPImagePath, 65);
+			imagedestroy($sourceImage);
+		}
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Convert To WebP for Magento2
 - Convert your product images to WebP format automatically on product save
 - Saved with .ngx.webp extension for serving WEBP with nginx conditionally. 
+
+```
+composer require hgati/convert-to-webp:dev-master
+```
